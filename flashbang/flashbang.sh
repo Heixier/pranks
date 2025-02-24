bdb4518a926768da6e3faa0c662eb4b4fe892947
Heixier <rzsiah@gmail.com>
2025-02-24 13:44:39 +0800

    add small delay before start

--- flashbang/flashbang.sh
+++ flashbang/flashbang.sh
@@ -3,11 +3,12 @@
 disco () {
 	count=25
 	xrandr -x -y
+	sleep 0.2
 	while (( $count > 0 )); do
 		if (( $count % 2 )) && (( $count != 1 )); then
 			xrandr --output HDMI-2 --brightness 0
 		else
-			xrandr --output HDMI-2 --brightness 10000
+			xrandr --output HDMI-2 --brightness 100000
 		fi;
 		sleep 0.1
 		count=$(( $count - 1 ))
