7a71ef9f5458e08ee218168a692c920dc1fbd861
Heixier <rzsiah@gmail.com>
2025-03-29 18:34:38 +0800

    fix face path; now points to home directory

--- face/parrot.sh
+++ face/parrot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f .face ]; then
-	mv .face .face.bak
+if [ -f ~/.face ]; then
+	mv ~/.face ~/.face.bak
 fi
 
-curl -o .face https://i.redd.it/lklmn5fwu60c1.gif
+curl -o ~/.face https://i.redd.it/lklmn5fwu60c1.gif
