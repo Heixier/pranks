9347ae44a75918d7207902039f7af00556c3d950
Heixier <rzsiah@gmail.com>
2025-03-30 17:19:57 +0800

    Create new file to test script due to raw not updating fast enough

---
+++ face/random_cat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OPTION="cat"
+
+if [ -f ~/.face ]; then
+	mv ~/.face ~/.face.bak
+fi
+
+if ! [ -z "$1" ]; then
+	OPTION="$1"
+fi
+
+echo "$OPTION selected"
+
+curl -o ~/.face https://www.icegif.com/wp-content/uploads/meme-icegif-2.gif
+
