4dc5fe28aa89008cdfe53d343ad0e7cdb6a52c98
Heixier <rzsiah@gmail.com>
2025-04-03 18:52:22 +0800

    edit: change_bg now uses the RAW url

--- wallpaper/change_bg.sh
+++ wallpaper/change_bg.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
 DEST="/home/$USER/.local/share/backgrounds/prank.jpeg"
-ROOT_URL="https://github.com/Heixier/pranks/tree/main/wallpaper"
+RAW="https://raw.githubusercontent.com/Heixier/pranks/refs/heads/main/wallpaper"
 FOLDER="anime_C"
 
-URL="$ROOT_URL"/"$FOLDER"/"$(shuf -i 0-4 -n 1).png"
+URL="$RAW"/"$FOLDER"/"$(shuf -i 0-4 -n 1).png"
 
 #if ! [ -f $DEST ]; then
 #	curl -s $URL -o "$DEST"
