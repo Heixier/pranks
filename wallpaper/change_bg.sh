672dfb9c55d53e8a52741b7313660072031b5ab1
Heixier <rzsiah@gmail.com>
2025-04-03 19:02:54 +0800

    added 2 more girls; fixed script

--- wallpaper/change_bg.sh
+++ wallpaper/change_bg.sh
@@ -2,14 +2,19 @@
 
 DEST="/home/$USER/.local/share/backgrounds/prank.jpeg"
 RAW="https://raw.githubusercontent.com/Heixier/pranks/refs/heads/main/wallpaper"
-FOLDER="anime_C"
 
-URL="$RAW"/"$FOLDER"/"$(shuf -i 0-4 -n 1).png"
+case "$1" in
+	"anime" | "anime_C")
+		FOLDER="anime_C";;
+	*)
+		FOLDER="anime_C";;
+esac
+
+URL="$RAW"/"$FOLDER"/"$(shuf -i 0-5 -n 1).png"
 
 #if ! [ -f $DEST ]; then
-#	curl -s $URL -o "$DEST"
+curl -s $URL -o "$DEST"
 #fi
 
-#gsettings set org.gnome.desktop.background picture-uri-dark "file://$DEST"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$DEST"
 
-printf "RANDOM URL: $URL\n"
