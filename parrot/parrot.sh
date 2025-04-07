fee3ca48f3a54687b9a4952368e1bcd53fdaa5dc
Heixier <rzsiah@gmail.com>
2025-04-07 16:46:11 +0800

    fix parrot grep

--- parrot/parrot.sh
+++ parrot/parrot.sh
@@ -6,12 +6,12 @@
 touch ~/.zshrc
 touch ~/.bashrc
 
-if ! ( cat ~/.zshrc | grep -q "curl parrot.live" )
+if ! ( cat ~/.zshrc | grep -q "^curl parrot.live" )
 then
 	echo "curl parrot.live" >> ~/.zshrc
 fi
 
-if ! ( cat ~/.bashrc | grep -q "curl parrot.live" )
+if ! ( cat ~/.bashrc | grep -q "^curl parrot.live" )
 then
 	echo "curl parrot.live" >> ~/.bashrc
 fi
