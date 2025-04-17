1a639b50883ae4aa1edac940fe643537d65df4fd
Heixier <rzsiah@gmail.com>
2025-04-17 17:35:35 +0800

    removed sandstorm audio port changing

--- audio/bg_play.sh
+++ audio/bg_play.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Note: YouTube tends to block streams
+# Note: YouTube tends to block streams; don't use YouTube
 URL="https://soundcloud.com/darude/sandstorm-radio-edit"
 
 # Store original values to reset to later
-original_volume="pactl get-sink-volume @DEFAULT_SINK@ | head -1 | awk '{ print $5 }'"
-original_port="pactl list sinks | grep "Active Port" | awk '{ print $3 }'"
+original_volume="$(pactl get-sink-volume @DEFAULT_SINK@ | head -1 | awk '{ print $5 }')"
+original_port="$(pactl list sinks | grep "Active Port" | awk '{ print $3 }')"
 
-# Make sure the audio plays
-
-# Sets the output to speaker so people on headphones don't die
-pactl set-sink-port @DEFAULT_SINK@ analog-output-speaker
+if ! [[ "$original_port" == "analog-output-speaker" ]]; then
+	printf "connect to speakers!\n"
+	exit 1
+fi
 
 # Unmute and max the volume
 pactl set-sink-mute @DEFAULT_SINK@ 0
 pactl set-sink-volume @DEFAULT_SINK@ 100% # can go up to 150%
 
-# If the argument provided is a number, then delay for that number.
-
+# Sleep for X number of seconds (if provided)
 timebomb () {
 	if [[ $1 ]] && [[ "$1" =~ ^[0-9]+$ ]]; then
 		if (( $1 > 420 )); then
@@ -34,9 +33,8 @@ cvlc --quiet --play-and-exit "$URL" 2>/dev/null &
 PID=$!
 sleep 6 # Hardcoded guess at download times
 
-# Set volume back to reasonable levels
+# Bring volume back down
 pactl set-sink-volume @DEFAULT_SINK@ 25%
 wait $PID
 
-pactl set-sink-port @DEFAULT_SINK@ "$original_port"
 pactl set-sink-volume @DEFAULT_SINK@ "$original_volume"
