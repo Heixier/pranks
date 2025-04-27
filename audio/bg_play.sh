05d942a6640c341e0424734281a066a5544cd5e6
Heixier <rzsiah@gmail.com>
2025-04-27 22:54:16 +0800

    moved timebomb timer to top before volume changes

--- audio/bg_play.sh
+++ audio/bg_play.sh
@@ -6,23 +6,6 @@ SAFE_MODE=1
 # Note: YouTube tends to block streams; don't use YouTube
 URL="https://github.com/Heixier/pranks/raw/refs/heads/main/audio/resources/discord_underwater.mp4"
 
-# Store original values to reset to later
-original_volume="$(pactl get-sink-volume @DEFAULT_SINK@ | head -1 | awk '{ print $5 }')"
-original_port="$(pactl list sinks | grep "Active Port" | awk '{ print $3 }')"
-
-if ! [[ "$original_port" == "analog-output-speaker" ]]; then
-	printf "connect to speakers!\n"
-	exit 1
-fi
-
-# Unmute and set the volume
-pactl set-sink-mute @DEFAULT_SINK@ 0
-if (( $SAFE_MODE == 1 )); then
-	pactl set-sink-volume @DEFAULT_SINK@ 50% # coward mode
-else
-	pactl set-sink-volume @DEFAULT_SINK@ 100% # can go up to 150%
-fi
-
 # Sleep for X number of seconds (if provided)
 timebomb () {
 	if [[ "$1" =~ ^[0-9]+$ ]]; then
@@ -34,7 +17,26 @@ timebomb () {
 	fi
 }
 
+# Store original values to reset to later
+original_volume="$(pactl get-sink-volume @DEFAULT_SINK@ | head -1 | awk '{ print $5 }')"
+original_port="$(pactl list sinks | grep "Active Port" | awk '{ print $3 }')"
+
+if ! [[ "$original_port" == "analog-output-speaker" ]]; then
+	printf "connect to speakers!\n"
+	exit 1
+fi
+
 timebomb "$@"
+
+# Unmute and set the volume
+pactl set-sink-mute @DEFAULT_SINK@ 0
+if (( $SAFE_MODE == 1 )); then
+	pactl set-sink-volume @DEFAULT_SINK@ 50% # coward mode
+else
+	pactl set-sink-volume @DEFAULT_SINK@ 100% # can go up to 150%
+fi
+
+
 
 cvlc --quiet --play-and-exit "$URL" 2>/dev/null &
 PID=$!
