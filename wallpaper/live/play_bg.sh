b15d37729869342cc7415cdcd07645c9e35d7ad6
Heixier <rzsiah@gmail.com>
2025-04-27 15:37:13 +0800

    testing autoplay again, changed matrix install directory

---
+++ wallpaper/live/play_bg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $1 ]]; then
+	FILE="$1"
+else
+	FILE="$HOME"/.local/share/backgrounds/live.mp4"
+
+$HOME/.local/bin/xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -- /usr/bin/cvlc --no-video-title-show --drawable-xid WID --loop --no-audio "$FILE"
--- wallpaper/live_bg.sh
+++ wallpaper/live_bg.sh
@@ -5,10 +5,11 @@ NAME="live"
 IMG_EXT="jpg"
 VID_EXT="mp4"
 AUTOSTART_FILE="autoplay.desktop"
+START_SCRIPT="play_bg.sh"
 
-dest="/home/$USER/.local/share/backgrounds"
+DEST="/home/$USER/.local/share/backgrounds"
 RAW="https://raw.githubusercontent.com/Heixier/pranks/refs/heads/main/wallpaper"
-folder="live"
+FOLDER="live"
 
 IMAGE="$NAME.$IMG_EXT"
 VIDEO="$NAME.$VID_EXT"
@@ -18,13 +19,18 @@ VIDEO_DEST="$DEST/$VIDEO"
 
 IMG_URL="$RAW"/"$FOLDER"/"$IMAGE"
 VID_URL="$RAW"/"$FOLDER"/"$VIDEO"
-AUTOSTART_URL="$RAW"/"$AUTOSTART_FILE"
+AUTOSTART_URL="$RAW"/"$FOLDER"/"$AUTOSTART_FILE"
+START_SCRIPT_URL="$RAW"/"$FOLDER"/"$START_SCRIPT"
+
+printf "Curling: Image: %s\nVideo: %s\nAuto: %s\n" "$IMG_URL" "$VID_URL" "$AUTOSTART_URL"
 
 curl -s "$IMG_URL" -o "$IMAGE_DEST"
 curl -s "$VID_URL" -o "$VIDEO_DEST"
-curl -s "$AUTOSTART_URL" -i "$HOME"/.config/autostart/"$AUTOSTART_FILE"
+curl -s "$AUTOSTART_URL" -o "$HOME"/.config/autostart/"$AUTOSTART_FILE"
+curl -s "$START_SCRIPT_URL" -o "$HOME"/.local/bin/"$START_SCRIPT"
 
 # Set image
+printf "Set image to %s\n" "$IMAGE_DEST"
 gsettings set org.gnome.desktop.background color-shading-type 'solid'
 gsettings set org.gnome.desktop.background picture-options 'zoom'
 
@@ -33,4 +39,9 @@ gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_DEST"
 
 # Start video
 
-xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -- cvlc --no-video-title-show --drawable-xid WID --loop --no-audio "$VIDEO_DEST"
+# First checks if xwinwrap is installed, if not install it 
+
+# use command -v
+
+printf "Trying to start video from %s\n" "$VIDEO_DEST"
+$HOME/.local/bin/xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -- /usr/bin/cvlc --no-video-title-show --drawable-xid WID --loop --no-audio "$VIDEO_DEST"
