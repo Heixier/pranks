850bb95f6e514f4b2f40918470ca4ba29ff1036c
Heixier <rzsiah@gmail.com>
2025-04-27 15:42:16 +0800

    fix typo

--- wallpaper/live/play_bg.sh
+++ wallpaper/live/play_bg.sh
@@ -3,6 +3,6 @@
 if [[ $1 ]]; then
 	FILE="$1"
 else
-	FILE="$HOME"/.local/share/backgrounds/live.mp4"
-
+	FILE="$HOME/.local/share/backgrounds/live.mp4"
+fi
 $HOME/.local/bin/xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -- /usr/bin/cvlc --no-video-title-show --drawable-xid WID --loop --no-audio "$FILE"
