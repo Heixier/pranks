3bf4e8c5d6441bf3b6e8bf155d26d4928cd644b5
Heixier <rzsiah@gmail.com>
2025-04-28 22:15:35 +0800

    change: downloads into /tmp instead to support looping

--- profile/wallpaper/live_bg.sh
+++ profile/wallpaper/live_bg.sh
@@ -2,18 +2,8 @@
 
 # Run this script with "install" to install the required files, otherwise it just launches
 
-# Don't mess up my custom config
-if [ "$USER" = "rsiah" ]; then
-	if ! [ "$1" = "force" ]; then
-		exit 0
-	else
-		printf "WARNING: will most likely mess up config!\n"
-		shift
-	fi
-fi
-
-NAME="toothless"
-FOLDER="live"
+RAW="https://raw.githubusercontent.com/Heixier/pranks/refs/heads/main"
+RESOURCE_FOLDER="profile/wallpaper/live"
 PREFIX="$HOME/.local"
 
 XWINWRAP="$PREFIX/bin/xwinwrap"
@@ -23,64 +13,71 @@ VLC="/usr/bin/vlc"
 XWINWRAP_FLAGS="-fs -fdt -ni -b -nf -un -o 1.0 --"
 VLC_FLAGS="--drawable-xid WID --no-video-title-show --loop --no-audio --crop=16:9"
 
-IMG_EXT="jpg"
-VID_EXT="mp4"
 AUTOSTART_FILE="autoplay.desktop"
 START_SCRIPT="play_bg.sh"
 
-DEST="$HOME/.local/share/backgrounds"
-RAW="https://raw.githubusercontent.com/Heixier/pranks/refs/heads/main/profile/wallpaper"
+# IMAGE
+IMAGE="toothless.jpg"
+IMAGE_DIR="$HOME/.local/share/backgrounds"
+IMAGE_DEST="$IMAGE_DIR/$IMAGE"
 
-IMAGE="$NAME.$IMG_EXT"
-VIDEO="$NAME.$VID_EXT"
+# VIDEO
+VIDEO="toothless.mp4"
+VID_DIR="/tmp"
+VID_DEST="$VID_DIR/.heix.mp4"
+VID_URL="$RAW"/"$RESOURCE_FOLDER"/"$VIDEO"
 VID_HEADER=()
-
-IMAGE_DEST="$DEST/$IMAGE"
-# VIDEO_DEST="$DEST/$VIDEO" # Deprecated, now streams instead of downloading
 
 AUTOSTART_DEST="$HOME/.config/autostart/$AUTOSTART_FILE"
 START_SCRIPT_DEST="$PREFIX/bin/$START_SCRIPT"
 
-# IMG_URL="$RAW"/"$FOLDER"/"$IMAGE" # Deprecated, now takes a screenshot with vlc instead
-VID_URL="$RAW"/"$FOLDER"/"$VIDEO"
-
-AUTOSTART_URL="$RAW"/"$FOLDER"/"$AUTOSTART_FILE"
+AUTOSTART_URL="$RAW"/"$RESOURCE_FOLDER"/"$AUTOSTART_FILE"
 
 CUSTOMER_SHEET="https://docs.google.com/spreadsheets/d/117zic5M9CddUo9iyPA8awxdDiExT4g0vkWbLS_CPH-w/export?exportFormat=csv"
 
 # Create destination directory if it doesn't exist
-if ! [ -d "$DEST" ]; then
-	mkdir -p "$DEST"
+if ! [ -d "$IMAGE_DIR" ]; then
+	mkdir -p "$IMAGE_DIR"
 fi
+
+# Don't mess up my custom config
+guard () {
+if [ "$USER" = "rsiah" ]; then
+	if ! [ "$1" = "force" ]; then
+		exit 0
+	else
+		printf "WARNING: will most likely mess up config!\n"
+		shift
+	fi
+fi
+}
 
 cleanup () {
 	rm -f "$IMAGE_DEST"
-	# rm -f "$VIDEO_DEST"
+	rm -f "$VID_DEST"
 	rm -f "$AUTOSTART_DEST"
 	rm -f "$START_SCRIPT_DEST"
 }
 
-# Changes vid url for customers
+# Helps customers install their custom video file instead of the default
 attend_to_customer () {
-	local vid_url="$(awk -v usr="$USER" -F',' '$1 ~ usr { print $2 }' <(curl -Ls "$CUSTOMER_SHEET"))"
+	local custom_vid_url="$(awk -v usr="$USER" -F',' '$1 ~ usr { print $2 }' <(curl -Ls "$CUSTOMER_SHEET"))"
 	local status
 
-	if ! [[ "$vid_url" ]]; then
+	if ! [[ "$custom_vid_url" ]]; then
+		download "$VID_URL" "$VID_DEST"
 		printf "Debug: %s is not a customer!\n" "$USER"
 		return 0
 	fi
 
-	if [[ "$vid_url" == *"moewalls.com"* ]]; then
-		VID_HEADER+=("-H" "'Referer: https://moewalls.com'")
+	if [[ "$custom_vid_url" == *"moewalls.com"* ]]; then
+		VID_HEADER+=("-H" "Referer: https://moewalls.com")
 	fi
 
-	status="$(curl -o /dev/null -sLw "%{http_code}" "$vid_url" "${VID_HEADER[@]}")"
-	if [ "$status" != "200" ]; then
-		printf "Invalid URL: %s\nAborting... %s\n" "$vid_url" "$status"
+	if ! curl -sL --fail "$custom_vid_url" "${VID_HEADER[@]}" -o "$VID_DEST" 2>/dev/null; then
+		printf "Fatal: invalid URL: %s\n" "$custom_vid_url"
 		cleanup
 		exit 1
-	else
-		VID_URL="$vid_url"
 	fi
 }
 
@@ -88,7 +85,7 @@ download () {
 	local url="$1"
 	local status="$(curl -o /dev/null -sLw "%{http_code}" "$url")"
 	if [ "$status" != "200" ]; then
-		printf "Invalid URL: %s\nAborting...\n" "$1"
+		printf "Fatal: invalid URL: %s\n" "$url"
 		cleanup
 		exit 1
 	else
@@ -115,50 +112,51 @@ install_xwinwrap () {
 	fi
 }
 
+main () {
+	# Perform checks
+	if ! command -v "$CVLC" >/dev/null; then
+		printf "Fatal: %s not found. Aborting...\n" "$CVLC"
+		cleanup
+		exit
+	fi
+
+	if ! command -v "$XWINWRAP" >/dev/null; then
+		install_xwinwrap
+	fi
+
+	# Stop all other running instances
+	killall -9 $VLC >/dev/null 2>&1
+	killall -9 $XWINWRAP >/dev/null 2>&1
+
+	# Install
+	attend_to_customer
+	download "$AUTOSTART_URL" "$AUTOSTART_DEST"
+
+	# Create script to start playback
+	rm "$START_SCRIPT_DEST" 2>/dev/null
+	sleep 0.1 # For some reason && did not work
+	printf "#!/bin/bash\n\nkillall -9 $VLC >/dev/null 2>&1\nkillall -9 $XWINWRAP >/dev/null 2>&1\n$XWINWRAP $XWINWRAP_FLAGS $CVLC $VLC_FLAGS $VID_DEST >/dev/null 2>&1 &\n" > "$START_SCRIPT_DEST"
+
+	chmod +x "$START_SCRIPT_DEST"
+
+	# Set image
+	gsettings set org.gnome.desktop.background color-shading-type 'solid'
+	gsettings set org.gnome.desktop.background picture-options 'zoom'
+
+	gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMAGE_DEST"
+	gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_DEST"
+
+	# Set shortcut to show desktop (Windows ftw)
+	gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d', '<Control><Alt>d', '<Control><Super>d']"
+
+	# Start video
+	$START_SCRIPT_DEST
+}
+
 if [ "$1" = "clean" ] || [ "$1" = "cleanup" ]; then
 	cleanup
 	exit
 fi
 
-# Perform checks
-
-if ! command -v "$CVLC" >/dev/null; then
-	printf "Fatal: %s not found. Aborting...\n" "$CVLC"
-	cleanup
-	exit
-fi
-
-if ! command -v "$XWINWRAP" >/dev/null; then
-	install_xwinwrap
-fi
-
-# Stop all other running instances
-killall -9 $VLC >/dev/null 2>&1
-killall -9 $XWINWRAP >/dev/null 2>&1
-
-# Install
-
-# download "$IMG_URL" "$IMAGE_DEST"
-# download "$VID_URL" "$VIDEO_DEST"
-attend_to_customer
-download "$AUTOSTART_URL" "$AUTOSTART_DEST"
-
-# Create script to start playback
-rm "$START_SCRIPT_DEST" 2>/dev/null
-sleep 0.1 # For some reason && did not work
-printf "#!/bin/bash\n\nkillall -9 $VLC >/dev/null 2>&1\nkillall -9 $XWINWRAP >/dev/null 2>&1\n$XWINWRAP $XWINWRAP_FLAGS $CVLC $VLC_FLAGS <(curl -sL \"$VID_URL\" ${VID_HEADER[*]}) >/dev/null 2>&1 &\n" > "$START_SCRIPT_DEST"
-
-chmod +x "$START_SCRIPT_DEST"
-
-# Set image
-gsettings set org.gnome.desktop.background color-shading-type 'solid'
-gsettings set org.gnome.desktop.background picture-options 'zoom'
-
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMAGE_DEST"
-gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_DEST"
-
-# Set shortcut to show desktop (Windows ftw)
-gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d', '<Control><Alt>d', '<Control><Super>d']"
-
-# Start video
-$START_SCRIPT_DEST
+guard
+main "$@"
